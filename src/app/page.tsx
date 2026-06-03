3a4f3401ebf4b85260f8c345d45ac0b4acc9d95d
odairwow <odair.josesilva@gmail.com>
2026-06-03 16:02:40 -0400

    Add home page

---
+++ src/app/page.tsx
@@ -0,0 +1,19 @@
+import Header from '@/components/Header'
+import Hero from '@/components/Hero'
+import Featured from '@/components/Featured'
+import LeadForm from '@/components/LeadForm'
+import Footer from '@/components/Footer'
+import WhatsAppButton from '@/components/WhatsAppButton'
+
+export default function Home() {
+  return (
+    <>
+      <Header />
+      <Hero />
+      <Featured />
+      <LeadForm />
+      <Footer />
+      <WhatsAppButton />
+    </>
+  )
+}
